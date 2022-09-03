72176b3c77aae7983a1fbc365e3d0189fcd81436
Yan Soares <yts.1995@gmail.com>
2022-09-03 13:40:09 -0300

    test: should not be able to get a non-existing secretary

--- tests/services/secretaries/get-secretaries-by-id-service.spec.ts
+++ tests/services/secretaries/get-secretaries-by-id-service.spec.ts
@@ -1,3 +1,4 @@
+import { RequestError } from '@/errors'
 import { SecretariesRepository } from '@/repositories'
 import { GetSecretariesByIdService } from '@/services/secretaries'
 import { secretaryModel } from '@/tests/mocks'
@@ -17,7 +18,17 @@ describe('GetSecretariesByIdService', () => {
       const response = await secretariesService.execute('any-id')
 
       expect(response).toStrictEqual(secretaryModel)
-      expect(secretariesRepository.findById).toHaveBeenCalledTimes(1)
+      expect(secretariesRepository.findById).toHaveBeenNthCalledWith(1, secretaryModel.id)
+    })
+
+    it('should not be able to get a non-existing secretary', async () => {
+      const error = new RequestError('Secretária não existe.')
+      const nonExistingId = 'non-existing-id'
+
+      const promise = secretariesService.execute(nonExistingId)
+
+      await expect(promise).rejects.toThrow(error)
+      expect(secretariesRepository.findById).toHaveBeenNthCalledWith(1, nonExistingId)
     })
   })
 })
