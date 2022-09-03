4a4fd3150c27a5124bbd13400d77e67fcae957f5
Yan Soares <yts.1995@gmail.com>
2022-09-03 16:20:19 -0300

    test: should be able to get a list of patients

---
+++ tests/services/patients/get-patients-service.spec.ts
@@ -0,0 +1,22 @@
+import { PatientsRepository } from '@/repositories'
+import { GetPatientsService } from '@/services/patients'
+
+import { patientModel } from '@/tests/mocks'
+
+describe('GetPatientsService', () => {
+  const patientsRepository = {} as PatientsRepository
+  const patientsService = new GetPatientsService(patientsRepository)
+
+  describe('execute', () => {
+    beforeEach(() => {
+      patientsRepository.get = jest.fn().mockResolvedValue([patientModel])
+    })
+
+    it('should be able to get a list of patients', async () => {
+      const response = await patientsService.execute()
+
+      expect(response).toStrictEqual([patientModel])
+      expect(patientsRepository.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
