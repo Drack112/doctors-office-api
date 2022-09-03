02beb32957f6b204574452d00a43303f7a463f75
Yan Soares <yts.1995@gmail.com>
2022-09-02 23:23:57 -0300

    test: should not be able to get a doctor

--- tests/services/get-doctor-by-idservice.spec.ts
+++ tests/services/get-doctor-by-idservice.spec.ts
@@ -1,5 +1,7 @@
+import { RequestError } from '@/errors'
 import { DoctorsRepository } from '@/repositories'
 import { GetDoctorsByIdService } from '@/services'
+import { doctorModel } from '@/tests/mocks'
 
 describe('GetDoctorByIdService', () => {
   const doctorsRepository = {} as DoctorsRepository
@@ -11,8 +13,19 @@ describe('GetDoctorByIdService', () => {
     })
 
     it('should be able to get a doctor', async () => {
+      doctorsRepository.findById = jest.fn().mockResolvedValue(doctorModel)
+
       await doctorsService.execute('any-id')
 
+      expect(doctorsRepository.findById).toHaveBeenCalledTimes(1)
+    })
+
+    it('should not be able to get a doctor', async () => {
+      const error = new RequestError('Médico não existe.')
+
+      const promise = doctorsService.execute('any-id')
+
+      await expect(promise).rejects.toThrow(error)
       expect(doctorsRepository.findById).toHaveBeenCalledTimes(1)
     })
   })
