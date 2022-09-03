3af8a690aee0eaf5fe9af437c199aa1eab79b804
Yan Soares <yts.1995@gmail.com>
2022-09-02 23:18:30 -0300

    test: should be able to get a list of doctors

---
+++ tests/services/get-doctors-service.spec.ts
@@ -0,0 +1,20 @@
+import { DoctorsRepository } from '@/repositories'
+import { GetDoctorsService } from '@/services'
+import { doctorModel } from '@/tests/mocks'
+
+describe('GetDoctorsService', () => {
+  const doctorsRepository = {} as DoctorsRepository
+  const doctorsService = new GetDoctorsService(doctorsRepository)
+
+  describe('execute', () => {
+    beforeEach(() => {
+      doctorsRepository.get = jest.fn().mockResolvedValue(doctorModel)
+    })
+
+    it('should be able to get a list of doctors', async () => {
+      await doctorsService.execute()
+
+      expect(doctorsRepository.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
