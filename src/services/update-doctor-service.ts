3a739c3544ee9ddec5096d1f3dde43d8eb6b7721
Yan Soares <yts.1995@gmail.com>
2022-09-02 23:07:15 -0300

    feat: should not be able to update non-existing doctor

--- src/services/update-doctor-service.ts
+++ src/services/update-doctor-service.ts
@@ -9,14 +9,12 @@ export class UpdateDoctorService {
   async execute (id: string, params: DoctorDTO): Promise<void> {
     const doctorExists = await this.doctorsRepository.findById(id)
     if (!doctorExists) throw new RequestError('Médico não existe.')
-    console.log('doctorExists', doctorExists)
     const doctor = new DoctorModel(params)
     const doctorToUpdate = {
       ...doctor,
       id: doctorExists.id,
       updated_at: new Date()
     }
-    console.log('doctorToUpdate', doctorToUpdate)
     await this.doctorsRepository.update(doctorToUpdate)
   }
 }
