f9a045ad130afcd19f2de648edbd02a99912ec9c
Yan Soares <yts.1995@gmail.com>
2022-09-02 23:13:45 -0300

    test: should not be able to delete a non-existing doctor

---
+++ tests/services/delete-doctor-service.spec.ts
@@ -0,0 +1,36 @@
+import { RequestError } from '@/errors'
+import { DoctorsRepository } from '@/repositories'
+import { DeleteDoctorService } from '@/services'
+
+import { doctorModel } from '@/tests/mocks'
+
+describe('DeleteDoctorService', () => {
+  const doctorsRepository = {} as DoctorsRepository
+  const doctorsService = new DeleteDoctorService(doctorsRepository)
+
+  describe('execute', () => {
+    beforeEach(() => {
+      doctorsRepository.findById = jest.fn()
+      doctorsRepository.delete = jest.fn()
+    })
+
+    it('should be able to delete a doctor successfully', async () => {
+      doctorsRepository.findById = jest.fn().mockResolvedValue(doctorModel)
+
+      await doctorsService.execute('any-id')
+
+      expect(doctorsRepository.findById).toHaveBeenNthCalledWith(1, 'any-id')
+      expect(doctorsRepository.delete).toHaveBeenNthCalledWith(1, 'any-id')
+    })
+
+    it('should not be able to delete a non-existing', async () => {
+      const error = new RequestError('Médico não existe.')
+
+      const promise = doctorsService.execute('any-id')
+
+      await expect(promise).rejects.toThrow(error)
+      expect(doctorsRepository.findById).toHaveBeenNthCalledWith(1, 'any-id')
+      expect(doctorsRepository.delete).not.toHaveBeenCalled()
+    })
+  })
+})
