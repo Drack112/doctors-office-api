08f12f0771ad57d90da45508c17c88a2064ada5c
Yan Soares <yts.1995@gmail.com>
2022-09-02 22:38:03 -0300

    feat: should not be able to create new doctor with existing cpf

--- src/services/create-doctor-service.ts
+++ src/services/create-doctor-service.ts
@@ -1,4 +1,5 @@
 import { DoctorDTO } from '@/dtos'
+import { RequestError } from '@/errors'
 import { DoctorModel } from '@/models'
 import { DoctorsRepository } from '@/repositories'
 
@@ -6,6 +7,11 @@ export class CreateDoctorService {
   constructor (private readonly doctorsRepository: DoctorsRepository) {}
 
   async execute (params: DoctorDTO): Promise<void> {
+    const { email, crm, cpf } = params
+    const doctorByEmail = await this.doctorsRepository.findByEmail(email)
+    const doctorByCRM = await this.doctorsRepository.findByCRM(crm)
+    const doctorByCPF = await this.doctorsRepository.findByCPF(cpf)
+    if (doctorByEmail ?? doctorByCRM ?? doctorByCPF) throw new RequestError('Médico já existe.')
     const doctor = new DoctorModel(params)
     await this.doctorsRepository.create(doctor)
   }
