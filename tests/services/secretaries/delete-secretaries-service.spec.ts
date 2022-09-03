dc466f3a74cc58a37db3a33cdde8db2db29abd52
Yan Soares <yts.1995@gmail.com>
2022-09-03 12:44:40 -0300

    test: should not be able to delete a non-existing secretary

--- tests/services/secretaries/delete-secretaries-service.spec.ts
+++ tests/services/secretaries/delete-secretaries-service.spec.ts
@@ -1,3 +1,4 @@
+import { RequestError } from '@/errors'
 import { SecretariesRepository } from '@/repositories'
 import { DeleteSecretariesService } from '@/services/secretaries'
 
@@ -20,6 +21,16 @@ describe('DeleteSecretariesService', () => {
 
       expect(secretariesRepository.findById).toHaveBeenNthCalledWith(1, doctorModel.id)
       expect(secretariesRepository.delete).toHaveBeenNthCalledWith(1, doctorModel.id)
+    })
+
+    it('should not be able to delete a non-existing secretary', async () => {
+      const error = new RequestError('Secretária não existe.')
+
+      const promise = secretariesService.execute('any-id')
+
+      await expect(promise).rejects.toThrow(error)
+      expect(secretariesRepository.findById).toHaveBeenNthCalledWith(1, 'any-id')
+      expect(secretariesRepository.delete).not.toHaveBeenCalled()
     })
   })
 })
