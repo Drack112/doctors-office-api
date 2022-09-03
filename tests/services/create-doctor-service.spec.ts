c3dd5120a8e770262bd8dc70533a70f1cf1e3d4a
Yan Soares <yts.1995@gmail.com>
2022-09-02 22:38:16 -0300

    test: should not be able to create new doctor with existing cpf

--- tests/services/create-doctor-service.spec.ts
+++ tests/services/create-doctor-service.spec.ts
@@ -1,3 +1,4 @@
+import { RequestError } from '@/errors'
 import { DoctorsRepository } from '@/repositories'
 import { CreateDoctorService } from '@/services'
 
@@ -20,15 +21,31 @@ describe('CreateDoctorService', () => {
   const doctorsService = new CreateDoctorService(doctorsRepository)
 
   describe('execute', () => {
-    it('should be able to create doctor', async () => {
+    beforeAll(() => {
       doctorsRepository.create = jest.fn()
+      doctorsRepository.findByEmail = jest.fn()
+      doctorsRepository.findByCRM = jest.fn()
+      doctorsRepository.findByCPF = jest.fn()
+    })
 
+    it('should be able to create new doctor successfully', async () => {
       await doctorsService.execute(mockDoctor)
 
       expect(doctorsRepository.create).toHaveBeenNthCalledWith(1, {
         ...doctorModel,
         updated_at: null
       })
+    })
+
+    it('should not be able to create new doctor with existing cpf', async () => {
+      doctorsRepository.findByCPF = jest.fn().mockResolvedValue(doctorModel)
+      const error = new RequestError('Médico já existe.')
+
+      const promise = doctorsService.execute(mockDoctor)
+
+      await expect(promise).rejects.toThrow(error)
+      expect(doctorsRepository.findByCPF).toHaveBeenNthCalledWith(1, doctorModel.cpf)
+      expect(doctorsRepository.create).not.toHaveBeenCalled()
     })
   })
 })
