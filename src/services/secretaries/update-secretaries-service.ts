351a4d6a10b508eceb1a5517c432d70aa9332341
Yan Soares <yts.1995@gmail.com>
2022-09-03 12:24:31 -0300

    feat: should not be able to update a non-existing secretary

--- src/services/secretaries/update-secretaries-service.ts
+++ src/services/secretaries/update-secretaries-service.ts
@@ -1,15 +1,18 @@
-import { DoctorDTO } from '@/dtos'
+import { SecretaryDTO } from '@/dtos'
+import { RequestError } from '@/errors'
 import { SecretaryModel } from '@/models'
 import { SecretariesRepository } from '@/repositories'
 
 export class UpdateSecretariesService {
   constructor (private readonly secretariesRepository: SecretariesRepository) {}
 
-  async execute (id: string, params: DoctorDTO): Promise<void> {
+  async execute (id: string, params: SecretaryDTO): Promise<void> {
+    const secretaryExists = await this.secretariesRepository.findById(id)
+    if (!secretaryExists) throw new RequestError('Secretária não existe.')
     const secretary = new SecretaryModel(params)
     const secretaryToUpdate = {
       ...secretary,
-      id: 'any-id',
+      id: secretaryExists.id,
       updated_at: new Date()
     }
     await this.secretariesRepository.update(secretaryToUpdate)
