76091066d7ae8bb0d9c04118e4c6c001a08ace2d
Yan Soares <yts.1995@gmail.com>
2022-09-02 22:49:43 -0300

    chore: add husky, mysql-connection and doctors migration

---
+++ src/repositories/migrations/1662169562909-create-doctors-table.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createDoctorsTable1662169562909 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'doctors',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'cpf',
+            type: 'varchar'
+          },
+          {
+            name: 'crm',
+            type: 'varchar'
+          },
+          {
+            name: 'speciality',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: null
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('doctors')
+  }
+}
