720062d2d4ac04614c127a20225e573fa9b50655
Yan Soares <yts.1995@gmail.com>
2022-09-03 12:37:08 -0300

    feat: should not be able to update a secretary with existing email/cpf

--- src/services/secretaries/update-secretaries-service.ts
+++ src/services/secretaries/update-secretaries-service.ts
@@ -7,6 +7,8 @@ export class UpdateSecretariesService {
   constructor (private readonly secretariesRepository: SecretariesRepository) {}
 
   async execute (id: string, params: SecretaryDTO): Promise<void> {
+    const { email, cpf } = params
+    await this.validateData(email, cpf)
     const secretaryExists = await this.secretariesRepository.findById(id)
     if (!secretaryExists) throw new RequestError('Secretária não existe.')
     const secretary = new SecretaryModel(params)
@@ -16,5 +18,11 @@ export class UpdateSecretariesService {
       updated_at: new Date()
     }
     await this.secretariesRepository.update(secretaryToUpdate)
+  }
+
+  private async validateData (email: string, cpf: string): Promise<void> {
+    const secretaryByEmail = await this.secretariesRepository.findByEmail(email)
+    const secretaryByCPF = await this.secretariesRepository.findByCPF(cpf)
+    if (secretaryByEmail ?? secretaryByCPF) throw new RequestError('Secretária já existe.')
   }
 }
