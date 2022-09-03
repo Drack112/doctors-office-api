43b0c19895fe02ccf5d06f4d64357eb2712ffb8a
Yan Soares <yts.1995@gmail.com>
2022-09-03 12:38:34 -0300

    refactor: improve data validation on create secretary

--- src/services/secretaries/create-secretaries-service.ts
+++ src/services/secretaries/create-secretaries-service.ts
@@ -8,10 +8,14 @@ export class CreateSecretariesService {
 
   async execute (params: SecretaryDTO): Promise<void> {
     const { email, cpf } = params
+    await this.validateData(email, cpf)
+    const secretary = new SecretaryModel(params)
+    await this.secretariesRepository.create(secretary)
+  }
+
+  private async validateData (email: string, cpf: string): Promise<void> {
     const secretaryByEmail = await this.secretariesRepository.findByEmail(email)
     const secretaryByCPF = await this.secretariesRepository.findByCPF(cpf)
     if (secretaryByEmail ?? secretaryByCPF) throw new RequestError('Secretária já existe.')
-    const secretary = new SecretaryModel(params)
-    await this.secretariesRepository.create(secretary)
   }
 }
