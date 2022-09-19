d5f7f899e34daf0db047b92a6e1faa0fc7487759
Yan Soares <yts.1995@gmail.com>
2022-09-19 12:55:26 -0300

    test: should be able to create new clinic successfully

---
+++ tests/services/clinics/create-clinics-service.spec.ts
@@ -0,0 +1,32 @@
+import { ClinicsRepository } from '@/repositories'
+import { CreateClinicsService } from '@/services/clinics'
+
+import { mockClinic, clinicModel } from '@/tests/mocks'
+
+jest.mock('node:crypto', () => ({
+  randomUUID: jest.fn().mockImplementation(() => 'any-id')
+}))
+
+jest
+  .useFakeTimers('modern')
+  .setSystemTime(new Date('2022-09-01T00:00:00.000Z'))
+
+describe('CreateClinicsService', () => {
+  const clinicsRepository = {} as ClinicsRepository
+  const clinicsService = new CreateClinicsService(clinicsRepository)
+
+  describe('execute', () => {
+    beforeAll(() => {
+      clinicsRepository.create = jest.fn()
+    })
+
+    it('should be able to create new clinic successfully', async () => {
+      await clinicsService.execute(mockClinic)
+
+      expect(clinicsRepository.create).toHaveBeenNthCalledWith(1, {
+        ...clinicModel,
+        updated_at: null
+      })
+    })
+  })
+})
