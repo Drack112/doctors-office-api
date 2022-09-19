350151815578515391daf3294f96251f6c35e6be
Yan Soares <yts.1995@gmail.com>
2022-09-19 14:28:52 -0300

    feat: add create-clinics-controller prod file

---
+++ src/main/controllers/clinics/create-clinics-controller.ts
@@ -0,0 +1,21 @@
+import { RequestError } from '@/errors'
+import { CreateClinicsService } from '@/services/clinics'
+
+import { Request, Response } from 'express'
+
+export class CreateClinicsController {
+  constructor (private readonly createClinicsService: CreateClinicsService) {}
+
+  async handle (req: Request, res: Response): Promise<void> {
+    try {
+      await this.createClinicsService.execute(req.body)
+      res.sendStatus(201)
+    } catch (error) {
+      if (error instanceof RequestError) {
+        res.status(400).json({ message: error.message })
+      } else {
+        res.status(500).json({ error })
+      }
+    }
+  }
+}
