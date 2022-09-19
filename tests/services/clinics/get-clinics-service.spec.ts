a56aafcb5c6377537898faee2205413781f5b156
Yan Soares <yts.1995@gmail.com>
2022-09-19 14:11:50 -0300

    test: should be able to get a list of clinics

---
+++ tests/services/clinics/get-clinics-service.spec.ts
@@ -0,0 +1,21 @@
+import { ClinicsRepository } from '@/repositories'
+import { GetClinicsService } from '@/services/clinics'
+
+import { clinicModel } from '@/tests/mocks'
+
+describe('GetClinicsService', () => {
+  const clinicsRepository = {} as ClinicsRepository
+  const clinicsService = new GetClinicsService(clinicsRepository)
+
+  describe('execute', () => {
+    beforeEach(() => {
+      clinicsRepository.get = jest.fn().mockResolvedValue([clinicModel])
+    })
+
+    it('should be able to get a list of clinics', async () => {
+      await clinicsService.execute()
+
+      expect(clinicsRepository.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
