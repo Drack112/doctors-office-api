a6ac411f2c6a84c9c61ac874dd5d6d8a08e55b82
Yan Soares <yts.1995@gmail.com>
2022-09-19 14:08:31 -0300

    test: should not be able to delete a non-existing clinic

--- tests/services/clinics/delete-clinics-service.spec.ts
+++ tests/services/clinics/delete-clinics-service.spec.ts
@@ -1,5 +1,7 @@
+import { RequestError } from '@/errors'
 import { ClinicsRepository } from '@/repositories'
 import { DeleteClinicsService } from '@/services/clinics'
+import { clinicModel } from '@/tests/mocks'
 
 describe('DeleteClinicsService', () => {
   const clinicsRepository = {} as ClinicsRepository
@@ -7,13 +9,26 @@ describe('DeleteClinicsService', () => {
 
   describe('execute', () => {
     beforeEach(() => {
+      clinicsRepository.findById = jest.fn()
       clinicsRepository.delete = jest.fn()
     })
 
-    it('should be able to delete a admin successfully', async () => {
+    it('should be able to delete a clinic successfully', async () => {
+      clinicsRepository.findById = jest.fn().mockResolvedValue(clinicModel)
+
       await clinicsService.execute('any-id')
 
       expect(clinicsRepository.delete).toHaveBeenNthCalledWith(1, 'any-id')
+    })
+
+    it('should not be able to delete a non-existing clinic', async () => {
+      const error = new RequestError('Clínica não existe.')
+
+      const promise = clinicsService.execute('any-id')
+
+      await expect(promise).rejects.toThrow(error)
+      expect(clinicsRepository.findById).toHaveBeenNthCalledWith(1, 'any-id')
+      expect(clinicsRepository.delete).not.toHaveBeenCalled()
     })
   })
 })
