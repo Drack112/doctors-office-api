9cf867dd111b6f4870174470d1457b4f8f6584cb
Yan Soares <yts.1995@gmail.com>
2022-09-19 13:05:15 -0300

    feat: should not be able to update non-existing clinic

--- src/services/clinics/update-clinics-service.ts
+++ src/services/clinics/update-clinics-service.ts
@@ -1,4 +1,5 @@
 import { ClinicDTO } from '@/dtos'
+import { RequestError } from '@/errors'
 import { ClinicModel } from '@/models'
 import { ClinicsRepository } from '@/repositories'
 
@@ -7,10 +8,11 @@ export class UpdateClinicsService {
 
   async execute (id: string, params: ClinicDTO): Promise<void> {
     const clinicExists = await this.clinicsRepository.findById(id)
+    if (!clinicExists) throw new RequestError('Clínica não existe.')
     const clinic = new ClinicModel(params)
     const clinicToUpdate = {
       ...clinic,
-      id: clinicExists!.id,
+      id: clinicExists.id,
       updated_at: new Date()
     }
     await this.clinicsRepository.update(clinicToUpdate)
