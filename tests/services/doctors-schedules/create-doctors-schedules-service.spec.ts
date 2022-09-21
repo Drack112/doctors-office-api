705c5bdbf2bd7ca9aa504b499b2caeb909f55863
Yan Soares <yts.1995@gmail.com>
2022-09-20 21:43:35 -0300

    test: should not be able to create new doctor-schedule if already has a schedule at the same time to same doctor

--- tests/services/doctors-schedules/create-doctors-schedules-service.spec.ts
+++ tests/services/doctors-schedules/create-doctors-schedules-service.spec.ts
@@ -1,3 +1,4 @@
+import { RequestError } from '@/errors'
 import { DoctorsSchedulesRepository } from '@/repositories'
 import { CreateDoctorsSchedulesService } from '@/services/doctors-schedules'
 
@@ -18,6 +19,7 @@ describe('CreateDoctorsSchedulesService', () => {
   describe('execute', () => {
     beforeAll(() => {
       doctorsSchedulesRepository.create = jest.fn()
+      doctorsSchedulesRepository.findExistantSchedule = jest.fn()
     })
 
     it('should be able to create new doctor-schedule successfully', async () => {
@@ -27,6 +29,17 @@ describe('CreateDoctorsSchedulesService', () => {
         ...doctorScheduleModel,
         updated_at: null
       })
+    })
+
+    it('should not be able to create new doctor-schedule if already has a schedule at the same time to same doctor', async () => {
+      doctorsSchedulesRepository.findExistantSchedule = jest.fn().mockResolvedValue(doctorScheduleModel)
+      const error = new RequestError('Horário de atendimento já cadastrado.')
+
+      const promise = doctorsSchedulesService.execute(mockDoctorSchedule)
+
+      await expect(promise).rejects.toThrow(error)
+      expect(doctorsSchedulesRepository.findExistantSchedule).toHaveBeenNthCalledWith(1, { ...mockDoctorSchedule })
+      expect(doctorsSchedulesRepository.create).not.toHaveBeenCalled()
     })
   })
 })
