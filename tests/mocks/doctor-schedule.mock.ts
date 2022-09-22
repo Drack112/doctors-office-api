48e19e2b5d54239e2f2d463c8a87e7cf4efbe40b
Yan Soares <yts.1995@gmail.com>
2022-09-22 09:44:07 -0300

    test: should not be able to create new doctor-schedule if already has a schedule at the same time to same doctor

--- tests/mocks/doctor-schedule.mock.ts
+++ tests/mocks/doctor-schedule.mock.ts
@@ -3,15 +3,12 @@ import { DoctorScheduleModel } from '@/models'
 import { DoctorScheduleDTO, StatusEnum } from '@/dtos'
 
 export const mockDoctorSchedule: DoctorScheduleDTO = {
-  doctorId: 'any-doctor-id',
-  date: '22/10/2022',
-  time: '15:00',
+  doctor_id: 'any-doctor-id',
+  schedules: [
+    { date: '22/10/2022', time: '15:00' },
+    { date: '22/10/2022', time: '15:00' }
+  ],
   status: StatusEnum.available
 }
 
-export const doctorScheduleModel: DoctorScheduleModel = {
-  id: 'any-id',
-  created_at: new Date('2022-09-01'),
-  updated_at: null,
-  ...mockDoctorSchedule
-}
+export const doctorScheduleModel = new DoctorScheduleModel()
--- tests/services/doctors-schedules/create-doctors-schedules-service.spec.ts
+++ tests/services/doctors-schedules/create-doctors-schedules-service.spec.ts
@@ -1,4 +1,5 @@
 import { RequestError } from '@/errors'
+import { DoctorScheduleModel } from '@/models'
 import { DoctorsSchedulesRepository } from '@/repositories'
 import { CreateDoctorsSchedulesService } from '@/services/doctors-schedules'
 
@@ -19,26 +20,26 @@ describe('CreateDoctorsSchedulesService', () => {
   describe('execute', () => {
     beforeAll(() => {
       doctorsSchedulesRepository.create = jest.fn()
-      doctorsSchedulesRepository.findExistantSchedule = jest.fn()
+      doctorsSchedulesRepository.findExistantSchedules = jest.fn()
     })
 
     it('should be able to create new doctor-schedule successfully', async () => {
       await doctorsSchedulesService.execute(mockDoctorSchedule)
+      const doctorScheduleModel = new DoctorScheduleModel()
 
-      expect(doctorsSchedulesRepository.create).toHaveBeenNthCalledWith(1, {
-        ...doctorScheduleModel,
-        updated_at: null
-      })
+      expect(doctorsSchedulesRepository.create).toHaveBeenNthCalledWith(1, [
+        ...doctorScheduleModel.setObject({ ...mockDoctorSchedule })
+      ])
     })
 
     it('should not be able to create new doctor-schedule if already has a schedule at the same time to same doctor', async () => {
-      doctorsSchedulesRepository.findExistantSchedule = jest.fn().mockResolvedValue(doctorScheduleModel)
+      doctorsSchedulesRepository.findExistantSchedules = jest.fn().mockResolvedValue(doctorScheduleModel)
       const error = new RequestError('Horário de atendimento já cadastrado.')
 
       const promise = doctorsSchedulesService.execute(mockDoctorSchedule)
 
       await expect(promise).rejects.toThrow(error)
-      expect(doctorsSchedulesRepository.findExistantSchedule).toHaveBeenNthCalledWith(1, { ...mockDoctorSchedule })
+      expect(doctorsSchedulesRepository.findExistantSchedules).toHaveBeenNthCalledWith(1, { ...mockDoctorSchedule })
       expect(doctorsSchedulesRepository.create).not.toHaveBeenCalled()
     })
   })
