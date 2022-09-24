af2b2cbc86c100ae50d1ab29ba0ff144fdc7fc1d
Yan Soares <yts.1995@gmail.com>
2022-09-24 10:14:52 -0300

    test: should not be able to create new schedule for an non-existing doctor

--- tests/services/schedules/create-schedules-service.spec.ts
+++ tests/services/schedules/create-schedules-service.spec.ts
@@ -1,8 +1,8 @@
 import { RequestError } from '@/errors'
-import { PatientsRepository, SchedulesRepository } from '@/repositories'
+import { DoctorsRepository, PatientsRepository, SchedulesRepository } from '@/repositories'
 import { CreateSchedulesService } from '@/services/schedules'
 
-import { mockSchedule, patientModel, scheduleModel } from '@/tests/mocks'
+import { doctorModel, mockSchedule, patientModel, scheduleModel } from '@/tests/mocks'
 
 jest.mock('node:crypto', () => ({
   randomUUID: jest.fn().mockImplementation(() => 'any-id')
@@ -15,16 +15,19 @@ jest
 describe('CreateSchedulesService', () => {
   const schedulesRepository = {} as SchedulesRepository
   const patientsRepository = {} as PatientsRepository
-  const schedulesService = new CreateSchedulesService(schedulesRepository, patientsRepository)
+  const doctorsRepository = {} as DoctorsRepository
+  const schedulesService = new CreateSchedulesService(schedulesRepository, patientsRepository, doctorsRepository)
 
   describe('execute', () => {
     beforeAll(() => {
       schedulesRepository.create = jest.fn()
       patientsRepository.findById = jest.fn()
+      doctorsRepository.findById = jest.fn()
     })
 
     it('should be able to create new schedule', async () => {
       patientsRepository.findById = jest.fn().mockResolvedValue(patientModel)
+      doctorsRepository.findById = jest.fn().mockResolvedValue(doctorModel)
 
       await schedulesService.execute(mockSchedule)
 
@@ -34,6 +37,17 @@ describe('CreateSchedulesService', () => {
     it('should not be able to create new schedule for an non-existing patient', async () => {
       patientsRepository.findById = jest.fn()
       const error = new RequestError('Paciente não cadastrado.')
+
+      const promise = schedulesService.execute(mockSchedule)
+
+      await expect(promise).rejects.toThrow(error)
+      expect(schedulesRepository.create).not.toHaveBeenCalled()
+    })
+
+    it('should not be able to create new schedule for an non-existing doctor', async () => {
+      patientsRepository.findById = jest.fn().mockResolvedValue(patientModel)
+      doctorsRepository.findById = jest.fn()
+      const error = new RequestError('Médico não cadastrado.')
 
       const promise = schedulesService.execute(mockSchedule)
 
