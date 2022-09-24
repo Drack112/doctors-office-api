4bf651fbecf25d2e35cf998cecbc17294f622e75
Yan Soares <yts.1995@gmail.com>
2022-09-24 10:11:03 -0300

    feat: should not be able to create new schedule for an non-existing patient

--- src/services/schedules/create-schedules-service.ts
+++ src/services/schedules/create-schedules-service.ts
@@ -1,12 +1,18 @@
 import { SchedulesDTO } from '@/dtos'
+import { RequestError } from '@/errors'
 import { ScheduleModel } from '@/models'
-import { SchedulesRepository } from '@/repositories'
+import { PatientsRepository, SchedulesRepository } from '@/repositories'
 
 export class CreateSchedulesService {
   constructor (
-    private readonly schedulesRepository: SchedulesRepository) {}
+    private readonly schedulesRepository: SchedulesRepository,
+    private readonly patientsRepository: PatientsRepository
+  ) {}
 
   async execute (params: SchedulesDTO): Promise<void> {
+    const { patientId } = params
+    const patient = await this.patientsRepository.findById(patientId)
+    if (!patient) throw new RequestError('Paciente não cadastrado.')
     const schedule = new ScheduleModel(params)
     await this.schedulesRepository.create(schedule)
   }
