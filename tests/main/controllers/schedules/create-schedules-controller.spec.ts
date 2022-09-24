255646fb43877ffde16569f7d4f3b5673df001ad
Yan Soares <yts.1995@gmail.com>
2022-09-24 10:25:28 -0300

    test: add create-schedules-controller test file

---
+++ tests/main/controllers/schedules/create-schedules-controller.spec.ts
@@ -0,0 +1,48 @@
+import { RequestError } from '@/errors'
+import { CreateSchedulesController } from '@/main/controllers/schedules'
+import { CreateSchedulesService } from '@/services/schedules'
+
+import { mockSchedule } from '@/tests/mocks'
+
+describe('CreateSchedulesController', () => {
+  const schedulesService = {} as CreateSchedulesService
+  const schedulesController = new CreateSchedulesController(schedulesService)
+  const req: any = { body: jest.fn() }
+  const res: any = { status: jest.fn().mockReturnThis(), sendStatus: jest.fn().mockReturnThis(), json: jest.fn().mockReturnThis() }
+
+  beforeAll(() => {
+    schedulesService.execute = jest.fn()
+    req.body = mockSchedule
+  })
+
+  describe('handle', () => {
+    it('should be able to create new schedule', async () => {
+      await schedulesController.handle(req, res)
+
+      expect(schedulesService.execute).toHaveBeenNthCalledWith(1, req.body)
+      expect(res.sendStatus).toHaveBeenNthCalledWith(1, 201)
+    })
+
+    it('should not be able to create new schedule', async () => {
+      const error = new RequestError('some-error')
+      schedulesService.execute = jest.fn().mockRejectedValue(error)
+
+      await schedulesController.handle(req, res)
+
+      expect(schedulesService.execute).toHaveBeenNthCalledWith(1, req.body)
+      expect(res.status).toHaveBeenNthCalledWith(1, 400)
+      expect(res.json).toHaveBeenNthCalledWith(1, { message: error.message })
+    })
+
+    it('should not be able to create a new schedule and must return some server error ', async () => {
+      const error = new Error('some-error')
+      schedulesService.execute = jest.fn().mockRejectedValue(error)
+
+      await schedulesController.handle(req, res)
+
+      expect(schedulesService.execute).toHaveBeenNthCalledWith(1, req.body)
+      expect(res.status).toHaveBeenNthCalledWith(1, 500)
+      expect(res.json).toHaveBeenNthCalledWith(1, { error })
+    })
+  })
+})
