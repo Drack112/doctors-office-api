9579037657e00199637d39d633f18032b6989d66
Yan Soares <yts.1995@gmail.com>
2022-09-24 10:25:13 -0300

    feat: add create-schedules-controller prod file

---
+++ src/main/controllers/schedules/create-schedules-controller.ts
@@ -0,0 +1,21 @@
+import { RequestError } from '@/errors'
+import { CreateSchedulesService } from '@/services/schedules'
+
+import { Request, Response } from 'express'
+
+export class CreateSchedulesController {
+  constructor (private readonly createSchedulesService: CreateSchedulesService) {}
+
+  async handle (req: Request, res: Response): Promise<void> {
+    try {
+      await this.createSchedulesService.execute(req.body)
+      res.sendStatus(201)
+    } catch (error) {
+      if (error instanceof RequestError) {
+        res.status(400).json({ message: error.message })
+      } else {
+        res.status(500).json({ error })
+      }
+    }
+  }
+}
