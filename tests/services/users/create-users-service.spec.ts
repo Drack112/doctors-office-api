45095099dfba932aed2dfd58dafb11debed615c2
Yan Soares <yts.1995@gmail.com>
2022-09-25 15:21:37 -0300

    test: create users with some validations

---
+++ tests/services/users/create-users-service.spec.ts
@@ -0,0 +1,49 @@
+import { RequestError } from '@/errors'
+import { UsersRepository } from '@/repositories'
+import { CreateUsersService } from '@/services/users'
+
+import { mockUser, userModel } from '@/tests/mocks'
+
+jest.mock('bcryptjs', () => ({
+  hashSync: jest.fn().mockImplementation(() => 'any-hashed-password')
+}))
+
+jest.mock('node:crypto', () => ({
+  randomUUID: jest.fn().mockImplementation(() => 'any-id')
+}))
+
+jest
+  .useFakeTimers('modern')
+  .setSystemTime(new Date('2022-09-01T00:00:00.000Z'))
+
+describe('CreateUsersService', () => {
+  const usersRepository = {} as UsersRepository
+  const usersService = new CreateUsersService(usersRepository)
+
+  describe('execute', () => {
+    beforeAll(() => {
+      usersRepository.create = jest.fn()
+      usersRepository.findByEmail = jest.fn()
+    })
+
+    it('should be able to create new user (admin) successfully', async () => {
+      await usersService.execute(mockUser)
+
+      expect(usersRepository.create).toHaveBeenNthCalledWith(1, {
+        ...userModel,
+        updated_at: null
+      })
+    })
+
+    it('should not be able to create new user (admin) with existing cpf/email', async () => {
+      usersRepository.findByEmail = jest.fn().mockResolvedValue(userModel)
+      const error = new RequestError('Usuário já existe.')
+
+      const promise = usersService.execute(mockUser)
+
+      await expect(promise).rejects.toThrow(error)
+      expect(usersRepository.findByEmail).toHaveBeenNthCalledWith(1, userModel.email)
+      expect(usersRepository.create).not.toHaveBeenCalled()
+    })
+  })
+})
