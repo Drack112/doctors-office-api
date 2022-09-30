272b1555e417d53e1966c6cc7b98c7125ba4efbc
Yan Soares <yts.1995@gmail.com>
2022-09-29 22:09:19 -0300

    refactor: reorganizing middlewares

--- src/main/routes/patients.routes.ts
+++ src/main/routes/patients.routes.ts
@@ -10,8 +10,7 @@ const getPatientsController = GetPatientsControllerFactory()
 const getPatientsByIdController = GetPatientsByIdControllerFactory()
 
 export default (router: Router): void => {
-  router.use(ensuredAuthenticated(), accessProfilePermission())
-  router.get('/patients', async (req, res) => getPatientsController.handle(req, res))
+  router.get('/patients', ensuredAuthenticated(), accessProfilePermission(), async (req, res) => getPatientsController.handle(req, res))
   router.get('/patients/:id', async (req, res) => getPatientsByIdController.handle(req, res))
   router.post('/patients', async (req, res) => createPatientsController.handle(req, res))
   router.patch('/patients/:id', async (req, res) => updatePatientsController.handle(req, res))
