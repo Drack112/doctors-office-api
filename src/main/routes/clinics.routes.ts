f06a2b438ee0713762c7094484e9991747d67d4d
Yan Soares <yts.1995@gmail.com>
2022-09-30 20:02:17 -0300

    chore: remove uuid route

--- src/main/routes/clinics.routes.ts
+++ src/main/routes/clinics.routes.ts
@@ -1,6 +1,5 @@
 import { CreateClinicsControllerFactory, UpdateClinicsControllerFactory, DeleteClinicsControllerFactory, GetClinicsControllerFactory, GetClinicsByIdControllerFactory } from '@/main/factories/controllers/clinics'
 import { accessProfilePermission, ensuredAuthenticated } from '@/middleware'
-import { randomUUID } from 'crypto'
 
 import { Router } from 'express'
 
@@ -11,11 +10,6 @@ const getClinicsController = GetClinicsControllerFactory()
 const getClinicsByIdController = GetClinicsByIdControllerFactory()
 
 export default (router: Router): void => {
-  router.get('/uuid', async (req, res) => {
-    const uuid = randomUUID()
-    res.json(uuid).end()
-  })
-
   router.get('/clinics', ensuredAuthenticated(), accessProfilePermission(), async (req, res) => getClinicsController.handle(req, res))
   router.get('/clinics/:id', async (req, res) => getClinicsByIdController.handle(req, res))
   router.post('/clinics', async (req, res) => createClinicsController.handle(req, res))
