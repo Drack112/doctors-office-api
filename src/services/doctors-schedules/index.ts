00aa1008581bd396787c71af70cc081c3dac52ad
Yan Soares <yts.1995@gmail.com>
2022-09-29 22:17:02 -0300

    chore: change file name

--- src/services/doctors-schedules/index.ts
+++ src/services/doctors-schedules/index.ts
@@ -1,3 +1,3 @@
 export * from './create-doctors-schedules-service'
-export * from './delete-doctor-schedules-service'
+export * from './delete-doctors-schedules-service'
 export * from './get-doctors-schedules-by-doctor-id-service'
