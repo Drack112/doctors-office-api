6d78d2bf87f05ae6dcdb20ddd7be1cfda7550138
Yan Soares <yts.1995@gmail.com>
2022-10-15 16:16:50 -0300

    test: increase test coverage

--- src/main/controllers/dashboard/dashboard-controller.ts
+++ src/main/controllers/dashboard/dashboard-controller.ts
@@ -3,11 +3,11 @@ import { DashboardService } from '@/services/dashboard'
 import { Request, Response } from 'express'
 
 export class DashboardController {
-  constructor (private readonly dashboradService: DashboardService) {}
+  constructor (private readonly dashboardService: DashboardService) {}
 
   async handle (_: Request, res: Response): Promise<void> {
     try {
-      const dashboard = await this.dashboradService.execute()
+      const dashboard = await this.dashboardService.execute()
       res.status(200).json(dashboard)
     } catch (error) {
       res.status(500).json({ error })
