cb6f98b5437cee5989d3d611b35540e149505c8b
Yan Soares <yts.1995@gmail.com>
2022-10-30 10:52:50 -0300

    chore: remove users-profiles feat

--- src/dtos/user-profile-dto.ts
+++
@@ -1,4 +0,0 @@
-export type UserProfileDTO = {
-  userId: string
-  profileId: string
-}
--- src/infra/repositories/user-profile-repository.ts
+++
@@ -1,4 +0,0 @@
-import { BaseRepository } from './base-repository'
-import { UserProfileEntity } from '@/infra/entities'
-
-export class UsersProfilesRepository extends BaseRepository<UserProfileEntity> {}
